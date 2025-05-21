a5511a32bf936881be89c5b919188ec3942b51a8
RaghavMittal2001 <raghavmittal1389@gmail.com>
2025-05-22 00:07:03 +0530

    like count

--- src/components/ClientComponents/LikeButton.tsx
+++ src/components/ClientComponents/LikeButton.tsx
@@ -1,29 +1,30 @@
-import { CREATE_LIKE, DELETE_LIKE } from '@/graphql/Queries';
-import {  useMutation } from '@apollo/client';
-import React, { useEffect } from 'react'
-import { FaHeart, FaRegHeart } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
+import { CREATE_LIKE, DELETE_LIKE } from "@/graphql/Queries";
+import { useMutation } from "@apollo/client";
+import React, { useEffect } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
-const LikeButton = ({selectedTweet}:{selectedTweet:TweetType}) => {
-  const LogginUser = useSelector((state:ReduxState)=>state.user);
-  const[createlike]=useMutation(CREATE_LIKE);
-  const[deletelike]=useMutation(DELETE_LIKE);
-  const[isliked, setIsLiked]=React.useState(false);
-  
-    useEffect(() => {
-      console.log("selectedTweet", selectedTweet);
+const LikeButton = ({ selectedTweet }: { selectedTweet: TweetType }) => {
+  const LogginUser = useSelector((state: ReduxState) => state.user);
+  const [createlike] = useMutation(CREATE_LIKE);
+  const [deletelike] = useMutation(DELETE_LIKE);
+  const [isliked, setIsLiked] = React.useState(false);
+
+  useEffect(() => {
+    console.log("selectedTweet", selectedTweet);
     const likedByUser = selectedTweet.likes?.some(
       (like: { user: { id: number } }) => like.user.id === LogginUser.id
     );
     setIsLiked(likedByUser);
   }, [selectedTweet, LogginUser.id]);
-  
-     const handlelike = async (e: React.MouseEvent<HTMLButtonElement>) => {
-      e.preventDefault();
-      
-      // Handle like functionality here
-      //check if user already liked the tweet
-     if(isliked){// if yes, remove the like
+
+  const handlelike = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    // Handle like functionality here
+    //check if user already liked the tweet
+    if (isliked) {
+      // if yes, remove the like
       deletelike({
         variables: {
           tweetId: selectedTweet?.id,
@@ -32,31 +33,37 @@ const LikeButton = ({selectedTweet}:{selectedTweet:TweetType}) => {
       });
       setIsLiked(false);
       console.log("Unlike the tweet");
+    } else {
+      // if not, create a like
+      await createlike({
+        variables: {
+          tweetId: selectedTweet?.id,
+          userId: LogginUser.id,
+        },
+      });
+      setIsLiked(true);
+      console.log("like the tweet");
     }
-     else{
-       // if not, create a like
-       await createlike({
-         variables: {
-           tweetId: selectedTweet?.id,
-           userId: LogginUser.id,
-          },
-        });
-        setIsLiked(true);
-        console.log("like the tweet");
-      }
-    };
+  };
   return (
     <div>
       <button
-                                   title="Like"
-                                   type="button"
-                                   onClick={handlelike}
-                                   className="text-gray-500 cursor-pointer rounded-full hover:text-red-600 transition duration-200"
-                                 >
-                                     {isliked ? <FaHeart color='red'/> : <FaRegHeart />}
-                                 </button>
+        title="Like"
+        type="button"
+        onClick={handlelike}
+        className="text-gray-500 cursor-pointer rounded-full hover:text-red-600 transition duration-200 flex flex-row"
+      >
+        {isliked ? <FaHeart className="mt-1" color="red" /> : <FaRegHeart className="mt-1" />}
+        {selectedTweet.likes?.length > 0 && (
+          <span
+            className={`text-sm text-gray-500 ml-1 mt-0.5 ${isliked ? " text-red-500" : ""}`}
+          >
+            {selectedTweet.likes.length}
+          </span>
+        )}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default LikeButton
+export default LikeButton;
